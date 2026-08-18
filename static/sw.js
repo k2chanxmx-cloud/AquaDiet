440df1ba9eab3df77d5b3ec11a1574234a8685a2
k2chanxmx-cloud <k2chan.xmx@gmail.com>
2026-08-18 17:14:22 +0900

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,48 @@
+const CACHE = "aquadiet-v1-3";
 
-const CACHE = "aquadiet-v1-2";
-const ASSETS = ["/", "/static/style.css", "/static/character.webp", "/static/manifest.json"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+const ASSETS = [
+  "/",
+  "/static/style.css",
+  "/static/character.webp",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
