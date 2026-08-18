b69c140d5cb6d5ba19f63183f74fb4b510829c12
k2chanxmx-cloud <k2chan.xmx@gmail.com>
2026-08-18 16:58:16 +0900

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "aquadiet-v1-1";
+const CACHE = "aquadiet-v1-2";
 const ASSETS = ["/", "/static/style.css", "/static/character.webp", "/static/manifest.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("fetch", e => {
